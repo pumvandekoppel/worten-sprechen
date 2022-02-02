18f50b536627f5e798428887dcbeec181887246e
pumvandekoppel <46265157+pumvandekoppel@users.noreply.github.com>
2022-02-02 16:31:21 +0100

    Create worten.js

---
+++ scripts/worten.js
@@ -0,0 +1,21 @@
+var i = 0;
+var wort = $("img").attr("id");
+var j = 42;
+
+$(".image").on("click","img", function() {
+	i++;
+	if (i % j > 0) {
+		i = i % j;
+		var imageAddress = "../../images/" + wort + "-images/" + wort + "-" + i + ".png";
+		$("#"+ wort).attr("src", imageAddress);
+		console.log($("#" + wort).attr("src"));
+	}
+	else {
+		var imageAddress = "../../images/" + wort + "-images/" + wort + "-" + j + ".png";
+		$("#"+ wort).attr("src",imageAddress);
+		console.log($("#" + wort).attr("src"));
+	}
+}
+);
+
+
