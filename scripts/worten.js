3629e3409889badbb25220df6bbf3dbcd1ff2214
pumvandekoppel <46265157+pumvandekoppel@users.noreply.github.com>
2022-02-02 17:38:06 +0100

    Update worten.js
    update imageAddress

--- scripts/worten.js
+++ scripts/worten.js
@@ -6,12 +6,12 @@ $(".image").on("click","img", function() {
 	i++;
 	if (i % j > 0) {
 		i = i % j;
-		var imageAddress = "../../images/" + wort + "-images/" + wort + "-" + i + ".png";
+		var imageAddress = "../../images/" + wort + "-" + i + ".png";
 		$("#"+ wort).attr("src", imageAddress);
 		console.log($("#" + wort).attr("src"));
 	}
 	else {
-		var imageAddress = "../../images/" + wort + "-images/" + wort + "-" + j + ".png";
+		var imageAddress = "../../images/ + wort + "-" + j + ".png";
 		$("#"+ wort).attr("src",imageAddress);
 		console.log($("#" + wort).attr("src"));
 	}
