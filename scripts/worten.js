71569ab4b5926713f71609def4e420fa3235d8bb
Pum van de Koppel <pumvandekoppel@gmail.com>
2022-02-04 02:52:59 +0000

    niet echt iets

--- scripts/worten.js
+++ scripts/worten.js
@@ -1,16 +1,25 @@
 var i = 1;
 var wort = $("img").attr("id");
-var limit;
-
-if (wort == "auto") {
-	limit = 42;
-}
-else if (wort == "hund") {
-	limit = 2;
-}
-else if (wort == "baum") {
-	 limit = 28;
-}
+const limit = {
+	auto: 45,
+	laufen: 842,
+	baum: 2,
+	hund: 2,
+	trettroller: 7,
+	quadrat: 2,
+	foto: 12,
+	kugel: 3,
+	strauch: 8,
+	verbot: 13,
+	penny: 2,
+	mobel: 9,
+	blumen: 1,
+	fleck: 4,
+	zahlen: 50,
+	schachspiel: 3,
+	graffiti: 11,
+	staude: 5
+};
 
 $(".image").on("hover", "img", function() {
 }
@@ -18,14 +27,14 @@ $(".image").on("hover", "img", function() {
 
 $(".image").on("click","img", function() {
 	i++;
-	if (i % limit > 0) {
-		i = i % limit;
+	if (i % limit[wort] > 0) {
+		i = i % limit[wort];
 		var imageAddress = "../images/" + wort + "/" + wort + "-" + i + ".jpg";
 		$("#"+ wort).attr("src", imageAddress);
 		console.log($("#" + wort).attr("src"));
 	}
 	else {
-		var imageAddress = "../images/" + wort + "/" + wort + "-" + limit + ".jpg";
+		var imageAddress = "../images/" + wort + "/" + wort + "-" + limit[wort] + ".jpg";
 		$("#"+ wort).attr("src",imageAddress);
 		console.log($("#" + wort).attr("src"));
 	}
